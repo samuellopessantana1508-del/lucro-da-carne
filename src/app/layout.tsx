aa45cb82d1c7c3f6ede997a9cd423a8b79e0dc36
samuellopessantana1508-del <samuellopessantana1508@gmail.com>
2026-08-26 17:57:59 -0300

    fix(tracking): load MetaPixel component client-side and keep GTM noscript; ensure pixel loads (use MetaPixel)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import AppProviders from "@/components/AppProviders";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import SiteFooter from "@/components/SiteFooter";
+import MetaPixel from "@/components/MetaPixel";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -61,12 +62,6 @@ export default function RootLayout({
           }}
         />
 
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s){if(f.fbq) return; n=f.fbq=function(){ n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments) }; if(!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0; t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s); }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1680836316346863'); fbq('track', 'PageView');`,
-          }}
-        />
       </head>
       <body className="min-h-full flex flex-col">
         {/* Google Tag Manager (noscript) */}
@@ -76,12 +71,8 @@ export default function RootLayout({
           }}
         />
 
-        {/* Meta Pixel NoScript */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1680836316346863&ev=PageView&noscript=1" />`,
-          }}
-        />
+        {/* Meta Pixel (client) - use component that loads via next/script */}
+        <MetaPixel />
 
         <ErrorBoundary>
           <AppProviders>
